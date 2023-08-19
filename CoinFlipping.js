e2a88d5903bab992a01c8c80aba08f2f25d2cd55
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-08-19 16:49:51 -0500

    implement five heads asynchronously

--- CoinFlipping.js
+++ CoinFlipping.js
@@ -6,27 +6,36 @@ const tossCoin = () => {
     return Math.random() > 0.5 ? "heads" : "tails";
 }
 
-const fiveHeadsSync = () => {
+const fiveHeadsASync = new Promise( (resolve, reject) => {
     let headsCount = 0;
-    let attempts = 0;
+    let attempts = 1;
     let output = "";
-    while (headsCount < 5) {
-        attempts++;
+    while ((headsCount < 5) && (attempts < 101)) {
         let result = tossCoin();
         output += `${result} `;
         (result === "heads") ?
-            headsCount++ :
-            headsCount = 0;
-        if ( attempts % 10 === 0 ) {
+        headsCount++ :
+        headsCount = 0;
+        if (attempts % 10 === 0) {
             output += "\n";
         }
+        attempts++;
+    } 
+    if (attempts < 100) {
+        resolve (`${output}\n\nIt took ${attempts} tries to flip five "heads"`);
+    } else {
+        reject (`${output}\n\nCould not get 5 Heads in a row after 100 attempts`);
     }
-    return `${output}\n\nIt took ${attempts} tries to flip five "heads"`;
-}
+});
 
 console.log("//////////////////////////////////////");
 console.log("//  Coin Flipping Application");
 console.log("//////////////////////////////////////\n");
 
-console.log(fiveHeadsSync());
-console.log("This is run after the fiveHeadsSync function completes");
+fiveHeadsASync
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+
+console.log("**********************************");
+console.log("When does this run now?");
+console.log("**********************************\n");
